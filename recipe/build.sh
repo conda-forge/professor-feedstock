6fe6d1c6b415216856daee1fd676c42b4289a9af
Adam Morris <adam.morris@cern.ch>
2025-02-05 17:04:38 +0100

    builds locally

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ./configure --with-eigen=${PREFIX} --prefix=${PREFIX}
+# The Makefile only adds Eigen if CPPFLAGS is unset
+# but CPPFLAGS is set (without Eigen) for other reasons
+CPPFLAGS="-I${PREFIX}/include/eigen3 $CPPFLAGS"
 make
 make install
 
