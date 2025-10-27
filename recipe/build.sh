8c3525097317268ccb2a2b16fd5e7b2b6096d85b
Adam Morris <15155249+admorris@users.noreply.github.com>
2025-10-27 10:28:30 +0100

    Try building without ROOT

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Hacky way to make ./configure pass: false resolves to /usr/bin/false
+# The build process should proceed without ROOT
+export ROOTCONFIG=false
 ./configure --with-eigen=${PREFIX} --prefix=${PREFIX}
 # The Makefile only adds Eigen if CPPFLAGS is unset
 # but CPPFLAGS is set (without Eigen) for other reasons
 CPPFLAGS="-I${PREFIX}/include/eigen3 $CPPFLAGS"
 make
 make install
-
