1d293bf0fd3e20af3b0568e47a58e6c62b5dc2f3
Adam Morris <15155249+admorris@users.noreply.github.com>
2026-06-07 20:56:07 +0200

    try again

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-./configure CXXSTD=c++14 --with-eigen=${PREFIX} --prefix=${PREFIX}
+CXXSTD=c++14 ./configure --with-eigen=${PREFIX} --prefix=${PREFIX}
 # The Makefile only adds Eigen if CPPFLAGS is unset
 # but CPPFLAGS is set (without Eigen) for other reasons
-CPPFLAGS="-I${PREFIX}/include/eigen3 $CPPFLAGS"
-make
+make CXXSTD=c++14 CPPFLAGS="-I${PREFIX}/include/eigen3 ${CPPFLAGS}"
 make install
